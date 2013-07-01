cb61a4c73b08dcdf0b1157567444cc4f6ab1262b
Hoat Le <hoatlevan@gmail.com>
2013-07-01 10:42:11 +0700

    Add git, fix depends

--- cookbooks/teracy-dev/metadata.rb
+++ cookbooks/teracy-dev/metadata.rb
@@ -6,7 +6,6 @@ description      'Installs/Configures teracy-dev'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'magic_shell'
 depends          'python'
 
 recipe "teracy-dev::virtualenvwrapper", "Installs virtualenvwrapper using the python_pip resource and configure it."
