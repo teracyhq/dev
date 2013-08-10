7be83dd49cfc0d54d8d9a02f16e5c972c62a47bf
Hoat Le <hoatlevan@gmail.com>
2013-08-10 06:43:22 +0000

    Fix multi branch auto docs build [#47]

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,9 +3,9 @@
 #setup travis-ci configuration basing one the being-built branch
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
-    export DEPLOY_HTML_DIR=''
+    export DEPLOY_HTML_DIR=docs
 else
     if [[ $TRAVIS_BRANCH == 'develop' ]] ; then
-        export DEPLOY_HTML_DIR='docs/develop'
+        export DEPLOY_HTML_DIR=docs/develop
     fi
 fi
