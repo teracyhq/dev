552a06d52904a3ad9f3b5112348148b7b5f56bed
Hoat Le <hoatlevan@gmail.com>
2014-05-22 17:39:49 +0700

    DEV-54 | upgrade vagarant and virtualbox support: update installation scripts for Ubuntu 12.04

--- scripts/setup_working_env_chef.sh
+++ scripts/setup_working_env_chef.sh
@@ -18,8 +18,8 @@ if [[ "$distributor_id" != *Ubuntu* ]]; then
 fi
 
 is_32_bit=true
-virtualbox_link=http://download.virtualbox.org/virtualbox/4.2.10/virtualbox-4.2_4.2.10-84104~Ubuntu~precise_i386.deb
-vagrant_link=http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_i686.deb
+virtualbox_link=http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~precise_i386.deb
+vagrant_link=https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_i686.deb
 
 function determine_32_64_bit() {
     local machine=$(uname -m)
@@ -32,8 +32,8 @@ determine_32_64_bit
 
 if ! $is_32_bit ; then
     echo "installing packages of 64-bit virtualbox and vagrant..."
-    virtualbox_link=http://download.virtualbox.org/virtualbox/4.2.10/virtualbox-4.2_4.2.10-84104~Ubuntu~precise_amd64.deb
-    vagrant_link=http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_x86_64.deb
+    virtualbox_link=http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~precise_amd64.deb
+    vagrant_link=https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb
 fi
 
 
@@ -44,14 +44,14 @@ function install_git() {
 function install_virtualbox() {
     cd /tmp
     wget $virtualbox_link
-    sudo dpkg -i virtualbox-4.2_4.2.10-84104~Ubuntu~precise*
+    sudo dpkg -i virtualbox-4.3_4.3.12-93733~Ubuntu~precise*
     cd -
 }
 
 function install_vagrant() {
     cd /tmp
     wget $vagrant_link
-    sudo dpkg -i vagrant_1.2.2*
+    sudo dpkg -i vagrant_1.6.2*
     cd -
 }
 
