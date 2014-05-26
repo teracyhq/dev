7dc52aa895014e07871a38dfacb3f56ccc948392
Hoat Le <hoatle@teracy.com>
2014-05-26 07:24:55 +0000

    update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'teracy-dev'
-copyright = u'2013, Teracy, Inc.'
+copyright = u'2013 - 2014, Teracy, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
