78c8f5b435bfeb9343fb642ef9f717994f91f051
vinhtran <vinhtran@teracy.com>
2014-05-29 09:02:32 +0700

    DEV-43 | Add more config for Java, Maven

--- main-cookbooks/teracy-dev/recipes/java.rb
+++ main-cookbooks/teracy-dev/recipes/java.rb
@@ -7,14 +7,14 @@
 #
 
 if node['teracy-dev']['java']['enabled']
-    node.default['teracy-dev']['accept_license_agreement'] = true
-    node.default['teracy-dev']['jdk_version'] = node['teracy-dev']['java']['version']
+    node.default['java']['accept_license_agreement'] = true
+    node.default['java']['jdk_version'] = node['teracy-dev']['java']['version']
 
     if node['teracy-dev']['java']['flavor'] == 'oracle'
-        node.default['teracy-dev']['oracle']['accept_oracle_download_terms'] = true
+        node.default['java']['oracle']['accept_oracle_download_terms'] = true
     end
 
-    node.default['teracy-dev']['install_flavor'] = node['teracy-dev']['java']['flavor']
+    node.default['java']['install_flavor'] = node['teracy-dev']['java']['flavor']
 
     include_recipe 'java::default'
 end
