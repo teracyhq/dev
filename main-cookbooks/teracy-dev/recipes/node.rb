93bedf490fe2dfd53ae884ded4ff9cb13576913e
Hoat Le <hoatle@teracy.com>
2014-06-15 17:02:51 +0000

    DEV-69 | upgrade npm

--- main-cookbooks/teracy-dev/recipes/node.rb
+++ main-cookbooks/teracy-dev/recipes/node.rb
@@ -6,16 +6,30 @@
 
 if node['teracy-dev']['nodejs']['enabled']
 
+    if !node['teracy-dev']['nodejs']['version'].strip().empty?
+        node.override['nodejs']['version'] = node['teracy-dev']['nodejs']['version']
+        node.override['nodejs']['checksum'] = node['teracy-dev']['nodejs']['checksum']
+        node.override['nodejs']['install_method'] = 'source'
+    end
+
     include_recipe 'nodejs'
 
-    node['teracy-dev']['nodejs']['npm'].each do |pkg|
+    if !node['teracy-dev']['nodejs']['npm']['version'].strip().empty?
+        node.override['nodejs']['npm'] = node['teracy-dev']['nodejs']['npm']['version']
+        include_recipe 'nodejs::npm'
+    end
+
+
+    node['teracy-dev']['nodejs']['npm']['globals'].each do |pkg|
+        #TODO(hoatle): need to check bin_path to make sure time saving?
         bin_path = '/usr/local/bin/'
-        if pkg == 'grunt-cli'
+        if pkg['name'] == 'grunt-cli'
             bin_path += 'grunt'
         else
-            bin_path += pkg
+            bin_path += pkg['name']
         end
-        npm_package pkg do
+        npm_package pkg['name'] do
+            version pkg['version']
             action :install
             not_if { ::File.exists?(bin_path) }
         end
