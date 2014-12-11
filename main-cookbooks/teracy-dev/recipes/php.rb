9beec50d466070873f0b835a5a574e4ed3f0a10d
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2014-12-11 12:32:29 +0700

    DEV-138 | Fix violations foodcritic of apache recipe

--- main-cookbooks/teracy-dev/recipes/php.rb
+++ main-cookbooks/teracy-dev/recipes/php.rb
@@ -15,7 +15,7 @@ if node['teracy-dev']['php']['enabled']
       bash 'remove php version if need' do
         code <<-EOF
           php_binary=$(which php);
-          php_version=$(php -r "@phpinfo();" | grep 'PHP Version' -m 1 | awk '{print $4}');
+          php_version=$(php -r '@phpinfo();' | grep 'PHP Version' -m 1 | awk '{print $4}');
 
           if [ "$php_version" != "#{node['teracy-dev']['php']['version']}" ] || [ ! -f /usr/lib/apache2/modules/mod_php5.so ]; then
               rm -R `ls -1 -d /var/chef/cache/php*/`
@@ -37,7 +37,7 @@ if node['teracy-dev']['php']['enabled']
       bash 'update apache2 php modules to source' do
         code <<-EOF
           mv /usr/lib/apache2/modules/libphp5.so /usr/lib/apache2/modules/mod_php5.so
-          echo "LoadModule php5_module        /usr/lib/apache2/modules/mod_php5.so" > /etc/apache2/mods-available/php5.load
+          echo 'LoadModule php5_module        /usr/lib/apache2/modules/mod_php5.so' > /etc/apache2/mods-available/php5.load
         EOF
         only_if 'ls -la /usr/lib/apache2/modules/libphp5.so'
         user 'root'
