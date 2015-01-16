4149e52ac5ccb9c4b72de371a285ab71960eb3f4
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2015-01-16 10:40:08 +0700

    DEV-170 | update automatic installation scripts

--- scripts/setup_working_env_chef.sh
+++ scripts/setup_working_env_chef.sh
@@ -3,23 +3,29 @@
 # Setup initial working environment for host machines. This initial script only serves Ubuntu.
 #
 # install git
-# install virtualbox-4.2.10
-# install vagrant 1.2.2
+# install virtualbox-4.3.4
+# install vagrant 1.7.2
 
 function command_exists() {
     type "$1" &> /dev/null;
 }
 
 distributor_id=`command_exists lsb_release && lsb_release -i`
+code_name=$(lsb_release -a | grep Codename | awk '{print $2}')
+vagrant_version="1.7.2"
 
 if [[ "$distributor_id" != *Ubuntu* ]]; then
     echo "You're not on Ubuntu. This script is currently provided to run under Ubuntu only."
     exit 1
 fi
 
+if [ "$code_name" == "trusty" ] || [ "$code_name" == "saucy" ] || [ "$code_name" == "utopic" ]; then
+    vbox_download_code_name="raring"
+fi
+
 is_32_bit=true
-virtualbox_link=http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~precise_i386.deb
-vagrant_link=https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_i686.deb
+virtualbox_link="http://download.virtualbox.org/virtualbox/4.3.8/virtualbox-4.3_4.3.8-92456~Ubuntu~${vbox_download_code_name}_i386.deb"
+vagrant_link="https://dl.bintray.com/mitchellh/vagrant/vagrant_${vagrant_version}_i686.deb"
 
 function determine_32_64_bit() {
     local machine=$(uname -m)
@@ -32,8 +38,8 @@ determine_32_64_bit
 
 if ! $is_32_bit ; then
     echo "installing packages of 64-bit virtualbox and vagrant..."
-    virtualbox_link=http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~precise_amd64.deb
-    vagrant_link=https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb
+    virtualbox_link="http://download.virtualbox.org/virtualbox/4.3.8/virtualbox-4.3_4.3.8-92456~Ubuntu~${vbox_download_code_name}_amd64.deb"
+    vagrant_link="https://dl.bintray.com/mitchellh/vagrant/vagrant_${vagrant_version}_x86_64.deb"
 fi
 
 
@@ -44,14 +50,16 @@ function install_git() {
 function install_virtualbox() {
     cd /tmp
     wget $virtualbox_link
-    sudo dpkg -i virtualbox-4.3_4.3.12-93733~Ubuntu~precise*
+    sudo dpkg -i virtualbox-4.3_4.3.8-92456~Ubuntu~*
+    sudo apt-get install -f
     cd -
 }
 
 function install_vagrant() {
     cd /tmp
     wget $vagrant_link
-    sudo dpkg -i vagrant_1.6.2*
+    sudo dpkg -i vagrant_*
+    sudo apt-get install -f
     cd -
 }
 
