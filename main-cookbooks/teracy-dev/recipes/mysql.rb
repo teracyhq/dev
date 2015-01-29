fd44ba666e164ba8d76ed4c3c07e5f472f167361
hoatle <hoatle@teracy.com>
2015-01-29 18:04:48 +0000

    DEV-176 | fix foodcritic violation

--- main-cookbooks/teracy-dev/recipes/mysql.rb
+++ main-cookbooks/teracy-dev/recipes/mysql.rb
@@ -9,28 +9,27 @@
 if node['teracy-dev']['mysql']['enabled']
 
     if !node['teracy-dev']['mysql']['password'].strip().empty?
-	    # set password
-	    %w(
-	        server_debian_password
-	        server_root_password
-	        server_repl_password
-	  	).each do |pwd|
-	      node.default['mysql'][pwd] = node['teracy-dev']['mysql']['password']
-		end
-	end
+        # set password
+        %w(
+            server_debian_password
+            server_root_password
+            server_repl_password
+        ).each do |pwd|
+          node.default['mysql'][pwd] = node['teracy-dev']['mysql']['password']
+        end
+    end
 
     node.default['mysql']['allow_remote_root'] = true
     node.default['mysql']['bind_address'] = '0.0.0.0'
 
     # force apt-get update
     # https://gist.github.com/lvnilesh/4039324/#comment-984780
-    if !mysql_installed?
-      execute 'compile-time-apt-get-update' do
+    execute 'compile-time-apt-get-update' do
         command 'apt-get update'
         ignore_failure true
         action :nothing
-      end.run_action(:run)
-    end
+        not_if { mysql_installed? }
+    end.run_action(:run)
 
     include_recipe 'mysql::server'
     include_recipe 'mysql::client'
