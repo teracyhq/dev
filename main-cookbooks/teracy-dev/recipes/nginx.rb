cd51d03f16d8507b4e53e6770d8ae6b32422041f
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2015-02-27 02:50:03 +0700

    DEV-143 | add support to use nginx instead of default apache server

---
+++ main-cookbooks/teracy-dev/recipes/nginx.rb
@@ -0,0 +1,48 @@
+#
+# Author:: Phuong <phuonglm@teracy.com>
+# Cookbook Name:: dev
+# Recipe:: nginx
+#
+# Copyright 2014, Teracy, Inc.
+#
+
+if node['teracy-dev']['nginx']['enabled']
+    node.override['nginx']['default_site_enabled'] = false
+    node.override['nginx']['source']['use_existing_user'] = true
+    node.override['nginx']['sendfile'] = 'off'
+    node.override['nginx']['install_method'] = 'source'
+    node.override['nginx']['init_style'] = "upstart"
+
+    node.override['nginx']['version'] = node['teracy-dev']['nginx']['version']
+    node.override['nginx']['default_root'] = node['teracy-dev']['nginx']['default_root']
+    node.override['nginx']['source']['prefix'] = "/opt/nginx"
+    node.override['nginx']['prefix'] = "/opt/nginx"
+
+    node.override['nginx']['user'] = 'vagrant'
+    node.override['nginx']['group'] = 'vagrant'
+
+    nginx_version = Mixlib::ShellOut.new('/opt/nginx/sbin/nginx -v 2>/dev/stdout | grep -o "[0-9]*\.[0-9]*\.[0-9]*"').run_command.stdout.gsub("\n","")
+
+    if nginx_version != node['teracy-dev']['nginx']['version']
+        bash 'clean up previous installed nginx' do
+            code <<-EOF
+                killall nginx || true
+                rm /opt/nginx* -rf
+                rm /etc/init.d/nginx* -rf
+                rm /etc/nginx/ -rf
+            EOF
+            user 'root'
+        end
+        node.from_file(run_context.resolve_attribute("nginx", "source"))
+        include_recipe 'nginx'
+    end
+    template "#{node['nginx']['dir']}/sites-available/default.conf" do
+        source 'nginx_site.erb'
+        owner 'vagrant'
+        group 'vagrant'
+        mode '0664'
+    end
+    link "#{node['nginx']['dir']}/sites-enabled/default.conf" do
+      to "#{node['nginx']['dir']}/sites-available/default.conf"
+    end
+end
--- main-cookbooks/teracy-dev/recipes/php.rb
+++ main-cookbooks/teracy-dev/recipes/php.rb
@@ -8,8 +8,10 @@
 
 if node['teracy-dev']['php']['enabled']
   if !node['teracy-dev']['php']['version'].strip().empty?
-      package 'apache2-dev' do
-        action :install
+      if node['teracy-dev']['apache']['enabled']
+        package 'apache2-dev' do
+          action :install
+        end
       end
 
       bash 'remove php version if need' do
@@ -28,27 +30,40 @@ if node['teracy-dev']['php']['enabled']
         only_if 'which php'
         user 'root'
       end
+
       node.override['php']['version'] = node['teracy-dev']['php']['version']
       node.override['php']['checksum'] = node['teracy-dev']['php']['checksum']
       node.override['php']['install_method'] = 'source'
-      node.override['php']['configure_options'] = ['--with-apxs2=/usr/bin/apxs2'] + node['php']['configure_options']
+
+      if node['teracy-dev']['apache']['enabled']
+        node.override['php']['configure_options'] = ['--with-apxs2=/usr/bin/apxs2'] + node['php']['configure_options']
+      end
 
      	include_recipe 'php'
-      bash 'update apache2 php modules to source' do
-        code <<-EOF
-          mv /usr/lib/apache2/modules/libphp5.so /usr/lib/apache2/modules/mod_php5.so
-          echo 'LoadModule php5_module        /usr/lib/apache2/modules/mod_php5.so' > /etc/apache2/mods-available/php5.load
-        EOF
-        only_if 'ls -la /usr/lib/apache2/modules/libphp5.so'
-        user 'root'
+
+      if node['teracy-dev']['apache']['enabled']
+        bash 'update apache2 php modules to source' do
+          code <<-EOF
+            mv /usr/lib/apache2/modules/libphp5.so /usr/lib/apache2/modules/mod_php5.so
+            echo 'LoadModule php5_module        /usr/lib/apache2/modules/mod_php5.so' > /etc/apache2/mods-available/php5.load
+          EOF
+          only_if 'ls -la /usr/lib/apache2/modules/libphp5.so'
+          user 'root'
+        end
+      end
+
+      if node['teracy-dev']['apache']['enabled']
+        package 'apache2-dev' do
+          action :install
+        end
       end
   else
     	include_recipe 'php'
-        if  node['teracy-dev']['mysql']['enabled']
-       		include_recipe 'php::module_mysql'
-       	end
-    end
-
+      if  node['teracy-dev']['mysql']['enabled']
+		include_recipe 'php::module_mysql'
+	end
+  end
+  if node['teracy-dev']['apache']['enabled']
     bash 'clean up apache mess' do
         code <<-EOF
             a2enmod php5 || true;
@@ -56,4 +71,21 @@ if node['teracy-dev']['php']['enabled']
         EOF
         user 'root'
     end
+  end
+  if node['teracy-dev']['nginx']['enabled']
+    apt_package 'php5-fpm' do
+      action :install
+    end
+    bash 'add php fpm' do
+      code <<-EOF
+        rm -rf /etc/php5/conf.d/pdo.ini
+        rm -rf /etc/php5/*/conf.d/pdo.ini
+        sed -i 's/NAME=php5-fpm/NAME=php-fpm/' /etc/init.d/php5-fpm
+        sed -i 's/DAEMON=\\/usr\\/sbin/DAEMON=\\/usr\\/local\\/sbin/' /etc/init.d/php5-fpm
+        killall php5-fpm
+        killall php-fpm
+        /etc/init.d/php5-fpm start
+      EOF
+    end
+  end
 end
