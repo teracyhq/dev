c2bcd1121de882a85b23c199b68b2498854e01f8
hoatle <hoatle@teracy.com>
2017-02-08 17:42:36 +0700

    @ #239 | should allow config override the object within an array: refactoring

--- lib/utility.rb
+++ lib/utility.rb
@@ -13,52 +13,56 @@ def overrides(obj1, obj2)
         obj1[key] = overrides(obj1[key], obj2[key])
       elsif value.class.name == 'Array'
         obj1_value = obj1[key].clone
-        value.map! do |val|
-          if val.class.name == 'Hash'
-            id_existing = false
-            obj1[key].each do |val1|
-              if val1['_id'] == val['_id']
-                id_existing = true
-                break
+        if value[0].class.name == 'String'
+          obj1_value = value
+        else
+          value.map! do |val|
+            if val.class.name == 'Hash'
+              id_existing = false
+              obj1[key].each do |val1|
+                if val1['_id'] == val['_id']
+                  id_existing = true
+                  break
+                end
               end
-            end
-            if id_existing == false
-              if !val['_op'].nil? and val['_op'] != 'a'
-                # warnings
-                puts yellow("_op = #{val['_op']} is invalid for non-existing id: #{val}")
+              if id_existing == false
+                if !val['_op'].nil? and val['_op'] != 'a'
+                  # warnings
+                  puts yellow("_op = #{val['_op']} is invalid for non-existing id: #{val}")
+                end
+                val['_op'] = 'a'
+              elsif val['_op'].nil?
+                val['_op'] = 'o'
               end
-              val['_op'] = 'a'
-            elsif val['_op'].nil?
-              val['_op'] = 'o'
-            end
 
-            if val['_op'] == 'a'
-              if val['_idx'].nil?
-                obj1_value.push(val)
-              else
-                obj1_value.insert(val['_idx'], val)
-              end
-            elsif val['_op'] == 'o'
-              obj1_value.map! do |val2|
-                if val2['_id'] == val['_id']
-                  val2 = overrides(val2, val)
+              if val['_op'] == 'a'
+                if val['_idx'].nil?
+                  obj1_value.push(val)
+                else
+                  obj1_value.insert(val['_idx'], val)
                 end
-                val2
-              end
-            elsif val['_op'] == 'r'
-              obj1_value.map! do |val3|
-                if val3['_id'] == val['_id']
-                  val3 = val
+              elsif val['_op'] == 'o'
+                obj1_value.map! do |val2|
+                  if val2['_id'] == val['_id']
+                    val2 = overrides(val2, val)
+                  end
+                  val2
                 end
-                val3
+              elsif val['_op'] == 'r'
+                obj1_value.map! do |val3|
+                  if val3['_id'] == val['_id']
+                    val3 = val
+                  end
+                  val3
+                end
+              elsif val['_op'] == 'd'
+                obj1_value.delete_if {|val4| val4['_id'] == val['_id'] }
               end
-            elsif val['_op'] == 'd'
-              obj1_value.delete_if {|val4| val4['_id'] == val['_id'] }
+            else
+              obj1_value = value
             end
-          else
-            obj1_value = value
+            val
           end
-          val
         end
         obj1[key] = obj1_value
       else
