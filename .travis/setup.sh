682194ad591ed37c0f348da2625f8ac27ac624e7
hoatle <hoatle@teracy.com>
2017-03-07 10:26:17 +0700

    @ #268 | should use vX.X.X from tag release for all the related version labels

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -4,10 +4,6 @@
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
     export DEPLOY_HTML_DIR=docs
-elif [[ $TRAVIS_BRANCH == 'develop' ]] ; then
-    export DEPLOY_HTML_DIR=docs/develop
-elif [[ $TRAVIS_BRANCH =~ ^v[0-9.]+$ ]]; then
-    export DEPLOY_HTML_DIR=docs/${TRAVIS_BRANCH:1}
 else
     export DEPLOY_HTML_DIR=docs/$TRAVIS_BRANCH
 fi
