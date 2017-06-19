482593a3dd0a188f47c057c2967c1a2b8a278ed2
Dat Phan <datphan0110@gmail.com>
2017-06-19 17:02:50 +0700

    @#295 | add docker host

--- provisioners/shells/clean-docker-auth.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-#! /bin/bash
-source ~/.profile
-
-echo "unset DOCKER_USERNAME_$1" >> ~/.profile
-
-echo "unset DOCKER_PASSWORD_$1" >> ~/.profile
--- provisioners/shells/setup-docker-auth.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-source ~/.profile
-
-echo "export DOCKER_USERNAME_$1=$2" >> ~/.profile
-
-echo "export DOCKER_PASSWORD_$1=$3" >> ~/.profile
-
-echo $DOCKER_USERNAME_0
-echo $DOCKER_USERNAME_1
