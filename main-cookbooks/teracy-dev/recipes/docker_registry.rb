eebb77232772e7fee13765c295b052c3161f771f
Dat Phan <datphan0110@gmail.com>
2017-07-13 11:04:18 +0700

    @#295 | fix tests

--- main-cookbooks/teracy-dev/recipes/docker_registry.rb
+++ main-cookbooks/teracy-dev/recipes/docker_registry.rb
@@ -9,7 +9,7 @@ docker_registry_conf = node['docker_registry']
 
 if docker_conf['enabled'] == true
     execute 'rm ~/.docker/config.json' do
-        command "rm /home/vagrant/.docker/config.json || true"
+        command 'rm /home/vagrant/.docker/config.json || true'
         only_if {
             docker_registry_conf['force'] == true and
             File.exist?('/home/vagrant/.docker/config.json')
@@ -19,9 +19,9 @@ if docker_conf['enabled'] == true
     docker_registry_conf['entries'].each.with_index do |entry, index|
         # private registry login
 
-        username = entry['username'] ? entry['username'] : ""
+        username = entry['username'] ? entry['username'] : ''
 
-        password = entry['password'] ? entry['password'] : ""
+        password = entry['password'] ? entry['password'] : ''
 
         if not username.empty? and not password.empty?
             opt = [
@@ -41,7 +41,7 @@ if docker_conf['enabled'] == true
     end
 
     execute 'copy /root/.docker/config.json to ~/.docker/config.json' do
-        command "cp /root/.docker/config.json /home/vagrant/.docker/config.json"
+        command 'cp /root/.docker/config.json /home/vagrant/.docker/config.json'
         only_if {
             File.exist?('/root/.docker/config.json') and (
                 docker_registry_conf['force'] == true or
