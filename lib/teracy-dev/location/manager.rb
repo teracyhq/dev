c58d92c17bc685948171914ebad19870199081fc
hoatle <hoatle@teracy.com>
2018-08-22 09:52:56 +0700

    @ #440 | clean up

--- lib/teracy-dev/location/manager.rb
+++ lib/teracy-dev/location/manager.rb
@@ -1,10 +1,8 @@
-require_relative '../util'
 require_relative 'git_synch'
 
 module TeracyDev
   module Location
     class Manager
-      @@logger = Logging.logger_for(self)
       @@synch_list = [GitSynch.new]
 
       def self.sync(location, sync_existing = true)
